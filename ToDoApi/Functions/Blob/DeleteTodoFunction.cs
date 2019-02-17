f471c1804de38dcb9cbff669f1fe426f5289d807
Cheranga Hatangala <cheranga@gmail.com>
2019-02-17 14:51:03 +1100

    Blobstorage - update and delete functions

---
+++ ToDoApi/Functions/Blob/DeleteTodoFunction.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace ToDoApi.Functions.Blob
+{
+    public static class DeleteTodoFunction
+    {
+        [FunctionName("BlobStorage_DeleteTodo")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "todo3/{id}")]
+            HttpRequest request,
+            [Blob("todos/{id}.json", Connection = "AzureWebJobsStorage")]
+            CloudBlockBlob blob,
+            ILogger logger)
+        {
+            logger.LogInformation("Deleting todo item");
+
+            var status = await blob.DeleteIfExistsAsync();
+
+            if (status)
+            {
+                return new OkResult();
+            }
+
+            return new NotFoundResult();
+        }
+    }
+}
---
+++ ToDoApi/Functions/Blob/UpdateTodoFunction.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+using ToDoApi.DTO;
+
+namespace ToDoApi.Functions.Blob
+{
+    public static class UpdateTodoFunction
+    {
+        [FunctionName("BlobStorage_UpdateTodo")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "todo3/{id}")]
+            HttpRequest request,
+            [Blob("todos/{id}.json", Connection = "AzureWebJobsStorage")]
+            CloudBlockBlob blob,
+            ILogger logger)
+        {
+            if (blob == null)
+            {
+                return new NotFoundResult();
+            }
+
+            var updatedContent = await new StreamReader(request.Body).ReadToEndAsync();
+            var updatedTodo = JsonConvert.DeserializeObject<TodoUpdateDto>(updatedContent);
+
+            var existingTodoContent = await blob.DownloadTextAsync();
+            var existingTodo = JsonConvert.DeserializeObject<TodoUpdateDto>(existingTodoContent);
+
+            if (!string.IsNullOrEmpty(updatedTodo.Description))
+            {
+                existingTodo.Description = updatedTodo.Description;
+            }
+
+            existingTodo.IsCompleted = updatedTodo.IsCompleted;
+
+            await blob.UploadTextAsync(JsonConvert.SerializeObject(existingTodo));
+
+            return new OkObjectResult(existingTodo);
+        }
+    }
+}
